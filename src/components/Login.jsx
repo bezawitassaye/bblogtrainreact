8d6e05c3e260124132007b9bde4804a4a57819c8
bezawitassaye <beza1221love@gmail.com>
2024-09-30 13:54:25 -0400

    complete the up part of userprofile page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -21,7 +21,7 @@ const Login = () => {
                 <p className='form__error-message'>This is an error message</p>
                  
                  <input type="email" placeholder='Email' name='email'
-                 value={userData.email} onChange={changeInputHandler} />
+                 value={userData.email} onChange={changeInputHandler}  autoFocus/>
 
                  <input type="password" placeholder='Password' name='password'
                  value={userData.password} onChange={changeInputHandler} />
--- src/components/Userprofile.jsx
+++ src/components/Userprofile.jsx
@@ -5,8 +5,14 @@ import { Link } from 'react-router-dom'
 import { FaCheck } from 'react-icons/fa'
 
 const Userprofile = () => {
-  const [avatar,setavatar] = useState("")
+  const [avatar,setavatar] = useState(avatars)
+  const [name,setName]=useState("")
+  const [email,setEmail] = useState("")
+  const [currentpassword,setcurrentpassword] = useState("")
+  const [newpassword,setnewpassword]= useState("")
+  const [confirmnewPasword,setconfirmnewpassword]=useState("")
   return (
+   <>
     <section className='profile'>
       <div className="container profile__container">
          <Link to={`/myposts/werwer`} className='btn'>
@@ -17,7 +23,7 @@ const Userprofile = () => {
          <div className="profile__detail">
             <div className="avatar__wrapper">
                <div className="profile__avatar">
-                  <img src={avatars} alt=''/>
+                  <img src={avatar} alt=''/>
                </div>
                {}
                <from className="avatar__form">
@@ -30,12 +36,43 @@ const Userprofile = () => {
                <button className='profile__avatar-btn'><FaCheck/></button>
             </div>
             <h1>Bezawit assaye</h1>
+           
          </div>
+         <div className="container">
+     
+           <form action="" className="form register__form ">
+             <p className='form__error-message'>This is an error message</p>
+             <input type="text" placeholder='Full Name' value={name} 
+               onChange={e=>setName(e.target.value)}/>
+             <input type="email" placeholder='Email' value={email} 
+               onChange={e=>setEmail(e.target.value)}/>
+             <input type="password" placeholder='current password' 
+               value={currentpassword} 
+               onChange={e=>setcurrentpassword(e.target.value)}/>
+             <input type="password" placeholder='new password ' value={newpassword} 
+               onChange={e=>setnewpassword(e.target.value)}/>
+             <input type="text" placeholder='confirm new password'
+                value={confirmnewPasword} 
+               onChange={e=>setconfirmnewpassword(e.target.value)}/>
+             
+
+              <button type='submit' className='btn primary'>Update details</button>
+             
+           </form>
+
+    </div>
 
       </div>
+    
+           
 
 
     </section>
+  
+    
+
+
+ </>
   )
 }
 
